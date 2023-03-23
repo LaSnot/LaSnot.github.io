34961a26cf9fcbb57d63a272dfc043cba55c472c
LaSnot <128653706+LaSnot@users.noreply.github.com>
2023-03-23 17:04:11 +0000

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ class _app {
     backgroundToggler = false;
     shouldIgnoreVideo = false;
     effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-    brandDescription = ['#1 discord mod', 'zurly#5555', 'white boy', 'curly head', '#1 skid', 'cool guy', 'pc nerd', '.gg/mp4'];
+    brandDescription = ['Colombian Snorter', 'Latino', 'Yet#0001', 'curly head', '#1 skid', 'cool guy', 'pc nerd', '.gg/mp4'];
   
     titleChanger = (text, delay) => {
       if (!text) return;
